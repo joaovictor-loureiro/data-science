b1f6799d1a21ef26bde7364eaa1b65b5fe144519
João Victor <56702506+joaovictor-loureiro@users.noreply.github.com>
2021-05-13 11:18:05 -0300

    UPDATE | Cursos
    - Scrapy

---
+++ data-science/cursos/scrapy/scrapy-quotes.py
@@ -0,0 +1,21 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'QuotesSpider'
+    start_urls = ['https://quotes.toscrape.com/']
+    
+    def parse(self, response):
+        quotes = response.xpath('*//div[@class="quote"]')
+        
+        for q in quotes:
+            yield {
+                'title': q.xpath('.//span[@class="text"]/text()').get(),
+                'author': q.xpath('.//small[@itemprop="author"]/text()').get(),
+                'tags': q.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').getall(),
+            }
+            
+            next_page =  response.xpath('*//li[@class="next"]/a/@href').get()
+            
+            if next_page is not None:
+                yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+            
